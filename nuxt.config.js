8e5283d4e5ccb15ab4c49ca4fe41284455f03954
nadeemnellicka <65375363+nadeemnellicka@users.noreply.github.com>
2020-07-19 11:48:44 +0530

    updated the api path

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-       baseURL: 'http://127.0.0.1:8000/',
+       baseURL: 'http://api.auth.ashdevs.in/',
   },
    auth: {
   strategies: {
